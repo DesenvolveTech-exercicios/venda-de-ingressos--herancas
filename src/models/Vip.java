c46ff9fa6cf65de7101f3b24567ed131e40d5352
Eduardo-LP <eduardolemospedroso@gmail.com>
2021-11-04 15:42:12 -0300

    inclui o ingresso vip e o test

---
+++ src/models/Vip.java
@@ -0,0 +1,10 @@
+package models;
+
+public class Vip extends Ingresso{
+    private final double valorAdicional = 30.50;
+
+    public void IngressoVip(){
+        double valorVip = Ingresso.valorIngresso + valorAdicional;
+        System.out.println(valorVip);
+    }
+}
--- src/test/TestIngresso.java
+++ src/test/TestIngresso.java
@@ -1,14 +1,23 @@
 package test;
 
 import models.Ingresso;
+import models.Vip;
 
 public class TestIngresso {
     public static void main(String[] args) {
+        System.out.println("------- ingresso normal--------");
         deveImprimirOValorDoIngresso();
+        System.out.println("------- ingresso Vip--------");
+        deveImprimirOValorDoIngressoVip();
     }
 
     public static void deveImprimirOValorDoIngresso(){
         Ingresso ingresso = new Ingresso();
         ingresso.imprimeValor();
+    }
+
+    public static void deveImprimirOValorDoIngressoVip(){
+        Vip vip = new Vip();
+        vip.IngressoVip();
     }
 }
